ae5ab7c915ccde48f76782e768fb93aff68aff18
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-24 15:26:49 +0200

    enhancing my profifolio web

--- backend/my-portifolio/src/pages/navbar.jsx
+++ backend/my-portifolio/src/pages/navbar.jsx
@@ -68,15 +68,15 @@ const NavBar = () => {
             </span>
           </motion.div>
 
-          <div className="hidden md:flex items-center gap-4">
+          <div className="hidden md:flex gap-4 ml-auto">
             {navItems.map(item => (
               <a
                 key={item.target}
                 href={`#${item.target}`}
-                className={`flex items-center gap-2 px-5 py-3 rounded-full text-lg font-medium transition-all duration-300 cursor-pointer
+                className={`flex items-center gap-2 px-5 py-3 rounded-full text-[15px] font-medium transition-all duration-300 cursor-pointer
                   ${activeSection === item.target
-                    ? "text-cyan-400 bg-gray-800/50 shadow-inner shadow-cyan-500/20"
-                    : "text-gray-300 hover:text-cyan-300 hover:bg-gray-800/30"
+                    ? "text-cyan-400 "
+                    : "text-gray-300 hover:text-cyan-300 hover:underline transition duration-500"
                   }`}
                 onClick={handleNavClick}
               >
