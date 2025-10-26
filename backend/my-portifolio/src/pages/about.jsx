e59dc2c0ff5ebadb3d422529328bde0c96f11eaa
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-26 09:51:19 +0200

    enhancing my profifolio web

--- backend/my-portifolio/src/pages/about.jsx
+++ backend/my-portifolio/src/pages/about.jsx
@@ -83,10 +83,10 @@ const About = () => {
             <div className="">
                 <h2 className="text-3xl text-center"><span className="text-cyan-500 font-bold" >My</span> <span className="font-bold">Hobbies</span></h2>
                 <div  className="flex justify-between w-full mt-9">
-                  <div className="space-y-3 bg-gray-700 w-[350px] p-3 border border-cyan-500 rounded-lg hover:scale-105 tansition duration-200 hover:shadow-lg hover:shadow-green-500">
+                  <div className="space-y-3 bg-gray-700 w-[350px] p-5 border border-cyan-500 rounded-lg hover:scale-105 tansition duration-200 hover:shadow-lg hover:shadow-green-500">
                     <div className="flex text-white text-sm ">
                        <p>Reading books</p>
-                       <p className="ms-[200px]">60%</p>
+                       <p className="ms-[185px]">60%</p>
                   </div>
                   <div className="bg-gray-500 rounded-full overflow-hidden">
                       <motion.div
@@ -98,10 +98,10 @@ const About = () => {
                         
                   </div>
                 </div>
-                <div className="space-y-3 bg-gray-700 w-[350px] p-3 border border-cyan-500 rounded-lg hover:scale-105 tansition duration-200 hover:shadow-lg hover:shadow-red-500">
+                <div className="space-y-3 bg-gray-700 w-[350px] p-5 border border-cyan-500 rounded-lg hover:scale-105 tansition duration-200 hover:shadow-lg hover:shadow-red-500">
                   <div className="flex text-white text-sm">
                       <p>Watching cartoons & cartoons</p>
-                      <p className="ms-[100px]">90%</p>
+                      <p className="ms-[90px]">90%</p>
                   </div>
                   <div className="bg-gray-500  rounded-full overflow-hidden">
                     <motion.div
@@ -115,10 +115,10 @@ const About = () => {
                </div>
               </div>
               <div className="flex justify-between w-full">
-                <div className="space-y-3 bg-gray-700 w-[350px] p-3 border border-cyan-500 rounded-lg mt-9 hover:scale-105 tansition duration-200 hover:shadow-lg hover:shadow-blue-500">
+                <div className="space-y-3 bg-gray-700 w-[350px] p-5 border border-cyan-500 rounded-lg mt-9 hover:scale-105 tansition duration-200 hover:shadow-lg hover:shadow-blue-500">
                    <div className="flex text-white text-sm">
                        <p>Listening musics</p>
-                       <p className="ms-[189px]">70%</p>
+                       <p className="ms-[170px]">70%</p>
                    </div>
                    <div className="bg-gray-500 rounded-full overflow-hidden">
                     <motion.div
