4c729f133f668ac760f97a0217a4afccdf336e04
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-27 14:43:12 +0200

    enhancing my profifolio web

--- backend/my-portifolio/src/pages/projects.jsx
+++ backend/my-portifolio/src/pages/projects.jsx
@@ -28,7 +28,7 @@ const Projects = () => {
         { 
             title: "Real-Time Chat Application",
             tech: [
-                { icon: <SiReact className="text-cyan-500 hover:text-white" />, name: "React", hover: "hover:bg-cyan-500 transition duration-200" },
+                { icon: <SiReact className="text-cyan-500" />, name: "React", hover: "hover:text-white hover:bg-cyan-500 transition duration-200 hover:text-black" },
                 { icon: <SiNodedotjs className="text-white" />, name: "Node.js", background: "bg-green-500" },
                 { icon: <FaDatabase className="text-white" />, name: "MySQL", background: "bg-yellow-500" },
             ],
@@ -192,7 +192,7 @@ const Projects = () => {
                                         {projects.tech.map((tech, i) => (
                                             <span 
                                             key={i}
-                                            className={`flex items-center gap-1 ${tech.hover} bg-gray-100/20 px-4 py-1 rounded-full text-sm`}
+                                            className={`flex items-center gap-1 ${tech.hover} bg-gray-100/20 px-4 py-1 rounded-full text-sm hover:text-white`}
                                             >
                                                 {tech.icon}
                                                 {tech.name}
