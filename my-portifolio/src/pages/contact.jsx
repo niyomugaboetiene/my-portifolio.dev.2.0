e21f34ead1ea56528159d284a2a8fad70ba22711
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-12-16 10:33:08 +0200

    adding some skills

--- my-portifolio/src/pages/contact.jsx
+++ my-portifolio/src/pages/contact.jsx
@@ -21,7 +21,7 @@ const Contact = () => {
     setIsSubmitting(true);
 
     try {
-      await axios.post("http://localhost:5000/send", formData);
+      await axios.post("https://niyomugaboetiene.onrender.com/send", formData);
       setSubmitStatus("success");
       setFormData({ name: "", email: "", message: "" });
     } catch (err) {
